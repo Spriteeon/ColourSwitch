3d69a1eeaf8f6489ee9eb0de4adc7ab0ce499847
Malachi Coward <v8009688@live.tees.ac.uk>
2020-11-10 16:27:25 +0000

    Initial commit

---
+++ ColourSwitch/GameViewController.swift
@@ -0,0 +1,30 @@
+//
+//  GameViewController.swift
+//  ColourSwitch
+//
+//  Created by COWARD, MALACHI (Student) on 10/11/2020.
+//  Copyright © 2020 COWARD, MALACHI (Student). All rights reserved.
+//
+
+import UIKit
+import SpriteKit
+
+class GameViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        if let view = self.view as! SKView? {
+            let scene = GameScene(size: view.bounds.size)
+            // Set the scale mode to scale to fit the window
+            scene.scaleMode = .aspectFill
+            // Present the scene
+            view.presentScene(scene)
+            
+            view.ignoresSiblingOrder = true
+            
+            view.showsFPS = true
+            view.showsNodeCount = true
+        }
+    }
+}
